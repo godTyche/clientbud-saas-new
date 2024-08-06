62dea76d8d2fa6fca95997e6eb4b3a6e5e3d7b35
godTyche <god.tyche124@gmail.com>
2024-08-06 07:37:22 -0400

    create account on email makreting and auto login

--- app/Listeners/NewUserListener.php
+++ app/Listeners/NewUserListener.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\NewUserEvent;
 use App\Notifications\NewUser;
 use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Http;
 
 class NewUserListener
 {
@@ -12,6 +13,35 @@ class NewUserListener
     public function handle(NewUserEvent $event)
     {
         Notification::send($event->user, new NewUser($event->user, $event->password));
+        $url = 'https://em.clientbud.com/public/api/v2/users/registerWithoutEmailVerification';
+        $full_name = explode(' ', $event->user->name);
+        $data = [
+            'email' => $event->user->email,
+            'first_name' => $full_name[0],
+            'last_name' => count($full_name) > 1 ? $full_name[1] : ' ',
+            'password' => 'mhCSaw%MSLqc',
+            'timezone' => 'UTC',
+            'language_id'   => 7
+        ];
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+
+        $response = curl_exec($ch);
+        $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        // Handle the response
+        if ($statusCode === 200) {
+            $data = json_decode($response, true);
+            // Do something with $data
+        } else {
+            // Handle the error
+            $error = $response;
+            // Do something with $error
+        }
     }
 
 }
--- resources/views/email-marketing/index.blade.php
+++ resources/views/email-marketing/index.blade.php
@@ -17,11 +17,8 @@ $addOrderPermission = user()->permission('add_order');
     <!-- CONTENT WRAPPER START -->
  
     <!-- CONTENT WRAPPER END -->
-    @if(in_array('admin', user()->roles->pluck('name')->toArray()))
-        <iframe src="https://em.clientbud.com/admin" style="width: 100%; height: 100vh;" frameborder="0"></iframe>
-    @else
-        <iframe src="https://em.clientbud.com" style="width: 100%; height: 100vh;" frameborder="0"></iframe>
-    @endif
+
+    <iframe src="https://em.clientbud.com/login?email={{user()->email}}&password=mhCSaw%MSLqc" style="width: 100%; height: 100vh;" frameborder="0"></iframe>
 
 @endsection
 
